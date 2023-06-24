c5b8abc8f7416b0af1d83ddb47ddcbc406989f62
Adnan <nepster112@gmail.com>
2023-06-24 09:21:56 +0100

    this change is from local main

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -14,6 +14,7 @@ When('user enter valid credentials', ()=>{
 Then('user should be able to login', ()=>{
     cy.get('.title').should('be.visible')
     cy.log("this is the third change i am making")
+    cy.log("this is stash pratice line")
 })
 
 Given('user is at home page', ()=>{
